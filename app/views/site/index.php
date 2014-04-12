148b86eb552cc461b151d34c9e3e62aaf88233e0
next768 <next7687@gmail.com>
2014-04-13 01:38:48 +0400

    Bacbon fixed

--- app/views/site/index.php
+++ app/views/site/index.php
@@ -1,1 +1,2 @@
-<script meta-data='scripts/main' src='<?php echo Yii::app()->request->baseUrl;?>/scripts/libs/require/require.js'></script>
+<script data-main='scripts/main' src='<?php echo Yii::app()->request->baseUrl;?>/scripts/libs/require/require.js'></script>
+<div class='content-backbone'></div>
