6a7dd70dc0c8a14b12f809684dc80d75bee0932b
Vladimir <vdaron@MacBook-Pro-Vladimir.local>
2014-04-13 19:47:37 +0400

    fixed

--- app/web/scripts/controllers/e_mark.js
+++ app/web/scripts/controllers/e_mark.js
@@ -9,12 +9,18 @@ angular.module('hackatonAApp')
     ];
         console.log($routeParams.id);
 
-        var ListCriteria=$resource("/api/criteria");
-
-        
+        var ListCriteria=$resource("/json/criteria");
         $scope.listCriteria=ListCriteria.query();
 
-        $scope.send_mark=function(){
-
+        $scope.proj = {
+            mark: ['user']
+        };
+        $scope.send=function(){
+            console.log($scope.mark)
         }
+        $scope.addCheck=function(index){
+
+            console.log(index)
+        }
+        //console.log(proj.mark);
   });
