9ee1f00b01cf10e2e7e50a7090f304d13e213395
Denis Mosolov <denismosolov@gmail.com>
2014-04-13 15:21:47 +0400

    Fonts

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -18,6 +18,8 @@
         <!-- endbuild -->
         <!-- build:css({.tmp,app}) styles/main.css -->
         <script src="scripts/libs/chart.min.js"></script>
+        <link href='http://fonts.googleapis.com/css?family=PT+Sans&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=PT+Sans+Caption&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="styles/main.css">
         <!-- endbuild -->
     </head>
