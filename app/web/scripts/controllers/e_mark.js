a1de387d9a89fd6011e5351e0d63f757902545d4
Vladimir <vdaron@MacBook-Pro-Vladimir.local>
2014-04-13 16:36:27 +0400

    fixed

--- app/web/scripts/controllers/e_mark.js
+++ app/web/scripts/controllers/e_mark.js
@@ -10,5 +10,26 @@ angular.module('hackatonAApp')
         console.log($routeParams.id);
 
         var ListCriteria=$resource("/api/criteria");
+
+        var ListProjCriteria=$resource("/api/ProjectCriteria",{},{
+            query: {method:'GET'},
+            post: {method:'POST'},
+            update: {method:'PUT'},
+            remove: {method:'DELETE'}
+        })
+
+        var result=ListCriteria.post({
+            criteria_id:1,
+            project_id: 2,
+            value: "2"
+        });
+        console.log(result);
+        
+        console.log(ListCriteria.query());
+
         $scope.listCriteria=ListCriteria.query();
+
+        $scope.send_mark=function(){
+
+        }
   });
