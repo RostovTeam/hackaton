9a135cf10ad379b94f008e45bf7a3a3620f6ebf9
Vladimir <vdaron@MacBook-Pro-Vladimir.local>
2014-04-13 20:42:43 +0400

    fixed

--- app/web/scripts/controllers/proj.js
+++ app/web/scripts/controllers/proj.js
@@ -11,9 +11,9 @@ angular.module('hackatonAApp')
         var Proj = $resource("/api/project/:proj_id",{proj_id:"@id"});
 
         var pr=Proj.get({proj_id:$routeParams.id});
-        console.log(pr);
+        console.log(pr.name);
 
-        
+
         $scope.commits_count=100;
         $scope.title=pr.name;
         $scope.des=pr.description;
