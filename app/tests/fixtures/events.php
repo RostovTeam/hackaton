950d60c601b488e838aa09c095eba1c28b7ce01f
komov.r <komov.r@gmail.com>
2014-10-26 20:29:53 +0400

    added fixtures

---
+++ app/tests/fixtures/events.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'event1' => [
+        'name' => 'best future development',
+        'stat_date' => '2014-12-01 17:00:00',
+        'end_date' => '2014-12-03 19:00:00'
+    ]
+];
---
+++ app/tests/fixtures/members.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'member1' => [
+        'full_name' => 'Elon Mask',
+        'phone' => '79054447777'
+    ]
+];
