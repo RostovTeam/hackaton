bdd9e62a842543572f1e2ce271c16f4bbbd2a7cb
komov.r <komov.r@gmail.com>
2014-11-05 06:10:06 +0400

    added install command

--- app/commands/InstallRolesCommand.php
+++ app/commands/InstallRolesCommand.php
@@ -11,31 +11,36 @@ class InstallRolesCommand extends CConsoleCommand
     public function actionIndex()
     {
         Yii::import('application.models.*');
-        
+
         $auth = Yii::app()->authManager;
         $auth->clearAll();
-        
+
         $role_member = $auth->createRole('member');
-        
-        $role_admin = $auth->createRole('admin');
-        $admin = new User('create');
-        $admin->login = 'admin';
-        $admin->password = 'password';
-        $admin->save();
-        $auth->assign('admin', $admin->id);
-        
-        $role_expert=$auth->createRole('expert');
-        $expert_user = new User('create');
-        $expert_user->login = '123';
+        $member = new Member('create');
+        $member->full_name = 'Larry Page';
+        $member->login = '89044440257';
+        $member->password = 'password';
+        $member->save();
+        $auth->assign('member', $member->id);
+
+
+        $role_admin = $auth->createRole('manager');
+        $manager = new Member('create');
+        $manager->full_name = 'Larry Page';
+        $manager->login = 'login';
+        $manager->password = 'manager_password';
+        $manager->save();
+        $auth->assign('manager', $manager->id);
+
+        $role_expert = $auth->createRole('expert');
+        $expert_user = new Member('create');
+        $expert_user->full_name = 'Larry Page';
+        $expert_user->login = '89044440257';
         $expert_user->password = 'password';
         $expert_user->save();
-        $expert= new Expert('create');
-        $expert->full_name='Expert';
-        $expert->user_id=$expert_user->id;
-        $expert->phone='123';
-        $expert->save();
         $auth->assign('expert', $expert_user->id);
 
         $auth->save();
     }
+
 }
