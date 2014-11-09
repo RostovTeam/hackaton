3455414c5e0524e178b1ed7d440e6c3b78501e6c
Vladimir Daron <vdaron@gmail.com>
2014-11-09 21:54:49 +0300

    all

--- app/controllers/ProjectCriteriaController.php
+++ app/controllers/ProjectCriteriaController.php
@@ -49,7 +49,7 @@ public function actionCreate()
         $this->transform($model);
         
         if (isset($model->expert_id) && isset($params['project_id']))
-                $modelname::model()->deleteAll('expert_id=:expert_id and project_id=:project_id and criteria_id-:criteria_id',
+                $modelname::model()->deleteAll('expert_id=:expert_id and project_id=:project_id and criteria_id=:criteria_id',
                     [':expert_id' => $model->expert_id, ':project_id' => $params['project_id'],':criteria_id'=>$params['criteria_id']]);
 
         if (!$model->validate())
